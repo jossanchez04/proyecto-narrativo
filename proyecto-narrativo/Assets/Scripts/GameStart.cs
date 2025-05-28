b6810d8eb5defc92937d668d10a1fac3edc96344
jossanchez04 <jos.sanchez@estudiantec.cr>
2025-05-27 21:19:42 -0600

    escenas iniciales y transiciones entre esas
    escena de bienvenida, créditos e instrucciones. además se añadieron a la lista de escenas

---
+++ proyecto-narrativo/Assets/Scripts/GameStart.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            Scene currentScene = SceneManager.GetActiveScene();
+
+            if (currentScene.name == "bienvenida") {
+
+                SceneManager.LoadScene("prejuego");
+            } 
+            else if (currentScene.name == "prejuego") {
+
+                SceneManager.LoadScene("instrucciones");
+            }
+            else if (currentScene.name == "instrucciones") {
+
+                SceneManager.LoadScene("SampleScene");
+            }
+
+        }
+    }
+}
